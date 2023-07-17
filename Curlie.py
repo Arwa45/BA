b5d96c44cb683ca10c1866d60ef5601c92f45cdf
Arwa45 <102920573+Arwa45@users.noreply.github.com>
2023-07-18 01:39:08 +0300

    curlie

--- Curlie.py
+++ Curlie.py
@@ -1,1 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+def extract_urls(url, visited_urls, depth):
+    if depth <= 0 or url in visited_urls:
+        return
+    
+    # Add the URL to visited_urls to avoid duplicate processing
+    visited_urls.add(url)
+    
+    try:
+        # Send an HTTP request to fetch the webpage content
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        # Parse the HTML content
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find all anchor tags and extract the href attribute (URLs)
+        urls = soup.find_all('a')
+        for link in urls:
+            href = link.get('href')
+            if href and href.startswith('http'):
+                # Convert relative URLs to absolute URLs
+                absolute_url = urljoin(url, href)
+                print(absolute_url)
+                
+                # Recursively extract URLs from sub-pages
+                extract_urls(absolute_url, visited_urls, depth - 1)
+                
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while fetching {url}: {str(e)}")
 
+# Initial URL to start extraction
+initial_url = "https://www.curlie.org"
+
+# Set the maximum depth for recursion
+max_depth = 3
+
+# Set to store visited URLs
+visited_urls = set()
+
+# Start the recursive extraction
+extract_urls(initial_url, visited_urls, max_depth)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BA
-A tool for finding and sketching loops in a graph (network) defined by edges in a CSV file (i.e., 2 columns, column A is the source node, column B is the target node, and a node is represented as an arbitrary string of characters).<br>
+A tool for extracting the URLs from either of www.curlie.org or www.wikipedia.org recursively (i.e., including sub-pages).<br>
 <i>This is an application of web scrapping using beautiful soup, which is a python library.</i>
